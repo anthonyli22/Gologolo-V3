7a8412dace6cd2b0c740594752c33181f89ab60a
Anthony Li <anthony20612000@gmail.com>
2020-04-06 02:23:15 -0400

    Updated database logo schema and GraphQL Logo API

--- server/models/Logo.js
+++ server/models/Logo.js
@@ -1,11 +1,17 @@
-var mongoose = require('mongoose');
+var mongoose = require("mongoose");
 
 var LogoSchema = new mongoose.Schema({
   id: String,
   text: String,
   color: String,
+  backgroundColor: String,
+  borderColor: String,
+  borderRadius: { type: Number, min: 2, max: 144 },
+  borderWidth: { type: Number, min: 2, max: 144 },
+  borderPadding: { type: Number, min: 2, max: 144 },
+  borderMargin: { type: Number, min: 2, max: 144 },
   fontSize: { type: Number, min: 2, max: 144 },
   lastUpdate: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model('Logo', LogoSchema);
+module.exports = mongoose.model("Logo", LogoSchema);
